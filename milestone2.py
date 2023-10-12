536547c091d76d04a283b27ba6e464308107db06
AyubM29 <ayyub6666@hotmail.com>
2023-10-12 17:33:52 +0100

    milestone2 changes

--- milestone2.py
+++ milestone2.py
@@ -5,4 +5,10 @@ word_list = ["Strawberries", "Apple", "Banana", "Orange", "Mango"]
 word = random.choice(word_list)
 print(word)
 
+guess = input("Choose one letter")
+if len(guess) == 1:
+    print("Good guess!")
+else: print("Oops! That is not a valid input.")
+
+
 
